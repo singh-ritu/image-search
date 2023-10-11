e22ee45595c105b0d69bb0ade8e14eaececc0687
Ritu <singhritu241101@gmail.com>
2023-10-11 18:23:41 +0530

    API call is integrated

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,23 @@ import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
+  const [response, setResponse] = useState(null);
 
   const handleInput = (e) => {
     setInput(e.target.value);
+  };
+
+  const getImages = () => {
+    console.log(input);
+    fetch(
+      `https://pixabay.com/api/?key=39975685-7f0a147f2a480877e9b622d71&q=${input}&image_type=photo`
+    )
+      .then((res) => res.json())
+
+      .then((data) => {
+        setResponse(data);
+        console.log(data);
+      });
   };
 
   return (
@@ -20,7 +34,9 @@ function App() {
           onChange={handleInput}
           value={input}
         ></input>
-        <button className="btn">Get Images</button>
+        <button className="btn" onClick={() => getImages()}>
+          Get Images
+        </button>
         <h3>{input + " " + "Images are:"} </h3>
       </div>
     </div>
